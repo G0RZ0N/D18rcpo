77b2e0d5ba8ec2e3bafa4ce3d34c698adebf47cf
IvanSakuta <vanasakuta@gmail.com>
2025-04-10 11:42:21 +0300

    My first commit

--- zd/Hotel.cs
+++ zd/Hotel.cs
@@ -10,6 +10,7 @@ namespace zd
     {
         public int id {  get; set; }
         public string Name { get; set; }
+
         public int CountOfStars {  get; set; }
         public string CountryName { get; set; }
         public string HotelImage {  get; set; }
