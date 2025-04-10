b27b40250b882f74368b304fe8ed6027599385ed
IvanSakuta <vanasakuta@gmail.com>
2025-04-10 13:29:14 +0300

    new commit_ff

--- zd/Hotel.cs
+++ zd/Hotel.cs
@@ -10,7 +10,7 @@ namespace zd
     {
         public int id {  get; set; }
         public string Name { get; set; }
-
+        // fjhdshff
         public int CountOfStars {  get; set; }
         public string CountryName { get; set; }
         public string HotelImage {  get; set; }
